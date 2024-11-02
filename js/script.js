8add28d091aa41268b48d63276af10be9979a2c0
Panha32 <panharavuth2526@gmail.com>
2024-11-02 22:08:19 +0700

    add action to webpage

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-let str = 'tos trip'
+const gotoSearch = (link) => {
+    let linkLocation = link.textContent;
+    sessionStorage.setItem("linkText", linkLocation);
+    location.href = 'search_result.html';
+}
